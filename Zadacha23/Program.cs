8c5e4d0604fae2d98654cc661e99c3765001522c
Nadya <Nadi-lv@yandex.ru>
2023-01-23 21:54:21 +0400

    Таблица кубов

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Таблица кубов");
+Console.WriteLine("Введите число N ");
+
+int num;
+int i = 1;
+
+while (!Int32.TryParse(Console.ReadLine(), out num))
+{
+    Console.WriteLine("Вы ввели не число. Повторите попытку: ");
+}
+cube (i, num);
+void cube (int i, int num)
+{
+    while (i < num+1)
+    {
+          Console.Write(Math.Pow(i,3) + "   ");
+          i++;
+    }
+
+}
